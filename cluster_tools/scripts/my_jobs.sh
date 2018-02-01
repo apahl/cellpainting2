367ca02bfe463f7694941897b2caa00057c88ecf
Axel Pahl <axelpahl@gmx.de>
2018-02-01 11:46:29 +0100

    only call sacct once!

--- cluster_tools/scripts/my_jobs.sh
+++ cluster_tools/scripts/my_jobs.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-COMPL=$(printf "%3d" $(sacct | grep COMPL | wc -l))
-RUNN=$(printf "%3d" $(sacct | grep RUNN | wc -l))
-PEND=$(printf "%3d" $(sacct | grep PEND | wc -l))
-FAIL=$(printf "%3d" $(sacct | grep FAIL | wc -l))
+OUTPUT=$HOME/sacct.txt
+sacct > $OUTPUT
+COMPL=$(printf "%3d" $(cat $OUTPUT | grep COMPL | wc -l))
+RUNN=$(printf "%3d" $(cat $OUTPUT | grep RUNN | wc -l))
+PEND=$(printf "%3d" $(cat $OUTPUT | grep PEND | wc -l))
+FAIL=$(printf "%3d" $(cat $OUTPUT | grep FAIL | wc -l))
 echo "Number of jobs completed: $COMPL"
 echo "               running:   $RUNN"
 echo "               pending:   $PEND"
